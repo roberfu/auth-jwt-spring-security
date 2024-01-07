79f210030b1f2f320f78acb57b9a0c79590e615e
Roberto Fuentes <roberfuentesg@gmail.com>
2024-01-07 01:33:52 -0300

    Init project

---
+++ src/main/java/cl/springmachine/auth/jwt/spring/security/AuthJwtSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package cl.springmachine.auth.jwt.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/springmachine/auth/jwt/spring/security/AuthJwtSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springmachine.auth.jwt.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
